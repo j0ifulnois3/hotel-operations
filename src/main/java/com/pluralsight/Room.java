f1d5c03fa23104cc6fcfbbc4f4a19a818947e31e
Joi McBrayer <j0ifulnois3@gmail.com>
2026-05-05 11:35:00 -0500

    Code Update

--- src/main/java/com/pluralsight/Room.java
+++ src/main/java/com/pluralsight/Room.java
@@ -3,24 +3,34 @@ package com.pluralsight;
 public class Room {
     private int numberOfbeds;
       private double price;
-      private boolean occupied;
-      private boolean dirty;
+      private boolean isOccupied;
+      private boolean isDirty;
 
     public Room(int numberOfBeds, double price, boolean occupied, boolean dirty) {
         this.numberOfbeds = numberOfBeds;
         this.price = price;
-        this.occupied = occupied;
-        this.dirty = dirty;
+        this.isOccupied = occupied;
+        this.isDirty = dirty;
     }
 
     public int getNumberOfBeds() { return numberOfbeds; }
     public double getPrice() { return price; }
-    public boolean isOccupied() { return occupied; }
-    public boolean isDirty() { return dirty; }
+    public boolean isOccupied() { return isOccupied; }
+    public boolean isDirty() { return isDirty; }
 
     // Derived Getter: Only available if NOT occupied AND NOT dirty
     public boolean isAvailable() {
-        return !this.occupied && !this.dirty;
+        return !this.isOccupied && !this.isDirty;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "numberOfbeds=" + numberOfbeds +
+                ", price=" + price +
+                ", isOccupied=" + isOccupied +
+                ", isDirty=" + isDirty +
+                '}';
     }
 }
 
